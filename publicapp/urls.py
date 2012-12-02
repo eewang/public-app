717406846f90f0ab8f9b846f485c9c65b7cf0f0a
Eugene Wang <eugene.wang.77@gmail.com>
2012-12-01 19:38:23 -0500

    started site wireframe

--- publicapp/urls.py
+++ publicapp/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'publicapp.views.home', name='home'),
-    # url(r'^publicapp/', include('publicapp.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('home.views',
+	url(r'^$', 'home'),
 )
